30e647449fe2db4be006561061f8bc9b7840d616
Willem Slaghekke <w.slaghekke@bytefusion.nl>
2017-06-18 12:58:06 +0200

    Add password reset link, use fos user registration controller

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\User;
+use FOS\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,7 +12,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 /**
@@ -19,19 +19,21 @@ use Vich\UploaderBundle\Form\Type\VichImageType;
  * @package AppBundle\Form
  * @author Willem Slaghekke <w.slaghekke@bytefusion.nl>
  */
-class UserRegistrationType extends AbstractType
+class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, ['label' => 'form.email', 'translation_domain' => 'FOSUserBundle'])
             ->add(
                 'plainPassword',
                 RepeatedType::class,
                 [
-                    'type'           => PasswordType::class,
-                    'first_options'  => ['label' => 'Password'],
-                    'second_options' => ['label' => 'Repeat password'],
+                    'type'            => PasswordType::class,
+                    'options'         => ['translation_domain' => 'FOSUserBundle'],
+                    'first_options'   => ['label' => 'form.password'],
+                    'second_options'  => ['label' => 'form.password_confirmation'],
+                    'invalid_message' => 'fos_user.password.mismatch',
                 ]
             )
             ->add('firstName', TextType::class)
@@ -46,14 +48,16 @@ class UserRegistrationType extends AbstractType
                 VichImageType::class,
                 [
                     'imagine_pattern' => 'compress_only',
+                    'required'=> false
                 ]
             )->add(
                 'coverPhotoFile',
                 VichImageType::class,
                 [
                     'imagine_pattern' => 'compress_only',
+                    'required'=> false
                 ]
-            )->add('submit', SubmitType::class);
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -65,5 +69,8 @@ class UserRegistrationType extends AbstractType
         );
     }
 
-
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
 }
