9c780916a99c8ec608e1df946df67e0d85231773
Max de Mooij <max.demooij@booking.com>
2017-10-22 01:32:10 +0200

    Styled the menu & added new colors to the overall style

--- web/script.js
+++ web/script.js
@@ -1,12 +1,22 @@
+$(document).ready(function(){
+  $('.navbar-right').click(function(){
+    $('.dropdown-menu').addClass('opening-menu-animation');
+    console.log('gelukt');
+  });
+
+  console.log('geladen');
+});
+
+
 $('.js-tilt').tilt({
     glare: true,
-    maxGlare: 1
+    maxGlare: 1,
     maxTilt:        5,
-    perspective:    500,   // Transform perspective, the lower the more extreme the tilt gets.
+    perspective:    100,   // Transform perspective, the lower the more extreme the tilt gets.
     easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
     scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
     speed:          300,    // Speed of the enter/exit transition.
     transition:     true,   // Set a transition on enter/exit.
     disableAxis:    null,   // What axis should be disabled. Can be X or Y.
     reset:          true,   // If the tilt effect has to be reset on exit.
-})
+});
