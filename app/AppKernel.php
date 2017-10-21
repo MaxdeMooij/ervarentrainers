3f82bdbd9ba0d1d1638328bfbbc8fe0c6f7ce00b
Max de Mooij <max.demooij@booking.com>
2017-10-21 18:13:26 +0200

    Gave up on the sitemap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,8 +22,6 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
-            // new Sonata\SeoBundle\SonataSeoBundle(),
-            // new Symfony\Cmf\Bundle\SeoBundle\CmfSeoBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
