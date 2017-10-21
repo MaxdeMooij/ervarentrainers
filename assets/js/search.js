af937251c087ca1fa58fa2224168225c2eef5568
Willem Slaghekke <willem.slaghekke@live.nl>
2017-10-22 01:28:47 +0200

    Implement basic search frontend

---
+++ assets/js/search.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import InstantSearch from 'vue-instantsearch';
+import App from './components/App.vue';
+
+Vue.use(InstantSearch);
+
+let app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            rootElem: null
+        }
+    },
+    beforeMount() {
+        console.log(this.$el);
+        this.rootElem = this.$el;
+    },
+    render: function(createElement) {
+        return createElement(App, {
+            props: {
+                appId: this.rootElem.getAttribute('app-id'),
+                apiKey: this.rootElem.getAttribute('api-key'),
+                indexName: this.rootElem.getAttribute('index-name'),
+            }
+        })
+    }
+});
