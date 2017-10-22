fa30df7d388c2823d0fae7fc6a61150667646dda
Max de Mooij <max.demooij@booking.com>
2017-10-22 20:50:29 +0200

    Added category tiles. Had no other placeholder image but my head, sorry for my presence.

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-  console.log('gelukt');
   $('.trainerblock').tilt({
     glare: true,
     maxGlare: 1,
     maxTilt:        5,
     perspective:    500,   // Transform perspective, the lower the more extreme the tilt gets.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
+    speed:          300,    // Speed of the enter/exit transition.
+    transition:     true,   // Set a transition on enter/exit.
+    disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,   // If the tilt effect has to be reset on exit.
+  });
+
+  $('.category').tilt({
+    glare: true,
+    maxGlare: 1,
+    maxTilt:        5,
+    perspective:    200,   // Transform perspective, the lower the more extreme the tilt gets.
     easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
     scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
     speed:          300,    // Speed of the enter/exit transition.
