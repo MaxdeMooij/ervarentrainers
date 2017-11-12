a32af6ebda83848d4767e111c5484a3e8cbb9871
Max de Mooij <max.demooij@booking.com>
2017-11-12 14:33:43 +0100

    Added a closing body tag and a footer layout. Pretty usefull stuff.

--- assets/js/components/App.vue
+++ assets/js/components/App.vue
@@ -4,65 +4,70 @@
             <div id="header">
                 <div class="headerbox">
                     <h1>Vind de ervaren trainers die bij je passen</h1>
-                    <div class="btn-group" role="group" aria-label="Search result type">
-                        <button type="button" class="btn" :class="[index == 'Training' ? 'btn-primary' : 'btn-outline-primary']" v-on:click="setIndex('Training')">Training</button>
-                        <button type="button" class="btn" :class="[index == 'User' ? 'btn-primary' : 'btn-outline-primary']" v-on:click="setIndex('User')">Trainers</button>
-                    </div>
                     <div class="search-area">
                         <input type="text" placeholder="Zoek een training..." class="search" v-model="query" v-on:keydown.enter="searchActive = true" />
-                        <div data-tilt class="search-button" v-on:click="searchActive = true">Zoeken</div>
+                        <div class="search-button" v-on:click="searchActive = true">Zoeken</div>
+
+                        <div class="btn-group" role="group" aria-label="Search result type">
+                            <button type="button" class="btn" :class="[index == 'Training' ? 'btn-primary' : 'btn-outline-primary']" v-on:click="setIndex('Training')">Training</button>
+                            <button type="button" class="btn" :class="[index == 'User' ? 'btn-primary' : 'btn-outline-primary']" v-on:click="setIndex('User')">Trainers</button>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="row" v-if="searchActive">
-                <div class="col-8">
-                <ais-no-results>
-                    <template slot-scope="{ query }">
-                        No products found for <i>{{ query }}</i>.
-                    </template>
-                </ais-no-results>
-                    <ais-results>
-                        <template slot-scope="{ result }">
-                            <div class="search_results">
-                                <div v-if="index == 'User'">
-                                    <h1>{{ result.firstName }} {{ result.lastName }}</h1>
-                                    <h4><ais-highlight :result="result" attribute-name="motto"></ais-highlight></h4>
-                                </div>
-                                <div class="search_result" v-if="index == 'Training'">
-                                    <a :href="'/trainers/' + result.user.slug">
-                                        <div class="avatar">
-                                            <img class="avatar" src="">
-                                            <div class="trainer">
-                                                <span>{{ result.user.firstName }} {{ result.user.lastName }}</span>
-                                                <span class="functie">{{ result.user.typeTrainer }} {{ result.user.lastName }}</span>
-                                            </div>
-                                        </div>
-                                    </a>
-                                    <a :href="'/trainers/' + result.slug">
-                                        <div class="content">
-                                            <h3 class="titel">{{ result.title }}</h3>
-                                            <p class="properties">
-                                                <span class="price">{{ result.prices[0].price }}</span>
-                                                <span class="timeframe">3 dagen</span>
-                                                <span class="type">{{ result.prices[0].type }}</span>
-                                            </p>
-                                            <p>{{ result.description }}</p>
-                                            <span class="tags">
-                                        <span v-for="tag in result.tags">{{ tag }}</span>
-                                    </span>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
+            <div class="container">
+
+
+              <div class="row" v-if="searchActive">
+                  <div class="col-8">
+                    <ais-no-results>
+                        <template slot-scope="{ query }">
+                            No products found for <i>{{ query }}</i>.
                         </template>
-                    </ais-results>
-                </div>
-                <div class="col-4">
-                    <ais-stats></ais-stats>
-                    <div v-if="index == 'Training'">
-                        <ais-price-range currency="€" attribute-name="searchPrice"></ais-price-range>
-                    </div>
-                    <ais-powered-by></ais-powered-by>
+                    </ais-no-results>
+                      <ais-results>
+                          <template slot-scope="{ result }">
+                              <div class="search_results">
+                                  <div v-if="index == 'User'">
+                                      <h1>{{ result.firstName }} {{ result.lastName }}</h1>
+                                      <h4><ais-highlight :result="result" attribute-name="motto"></ais-highlight></h4>
+                                  </div>
+                                  <div class="search_result" v-if="index == 'Training'">
+                                      <a :href="'/trainers/' + result.user.slug">
+                                          <div class="avatar">
+                                              <img class="avatar" src="">
+                                              <div class="trainer">
+                                                  <span>{{ result.user.firstName }} {{ result.user.lastName }}</span>
+                                                  <span class="functie">{{ result.user.typeTrainer }} {{ result.user.lastName }}</span>
+                                              </div>
+                                          </div>
+                                      </a>
+                                      <a :href="'/trainers/' + result.slug">
+                                          <div class="content">
+                                              <h3 class="titel">{{ result.title }}</h3>
+                                              <p class="properties" v-if="result.prices.length[0]">
+                                                  <span class="price">{{ result.prices[0].price }}</span>
+                                                  <span class="timeframe">3 dagen</span>
+                                                  <span class="type">{{ result.prices[0].type }}</span>
+                                              </p>
+                                              <p>{{ result.description }}</p>
+                                              <span class="tags">
+                                          <span v-for="tag in result.tags">{{ tag }}</span>
+                                      </span>
+                                          </div>
+                                      </a>
+                                  </div>
+                              </div>
+                          </template>
+                      </ais-results>
+                  </div>
+                  <div class="col-4">
+                      <ais-stats></ais-stats>
+                      <div v-if="index == 'Training'">
+                          <ais-price-range currency="€" attribute-name="searchPrice"></ais-price-range>
+                      </div>
+                      <ais-powered-by></ais-powered-by>
+                  </div>
                 </div>
             </div>
         <!--</ais-index>-->
